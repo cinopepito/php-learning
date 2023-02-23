f28c8e6280e1679597583fdaccd4dc4b80e2a595
cinopepito <83885361+cinopepito@users.noreply.github.com>
2023-02-23 23:57:57 +0100

    comment has been added

--- inst.php
+++ inst.php
@@ -1,3 +1,19 @@
 <?php
-echo '<strong>hello world !</strong>';
+echo '<strong>hello world !</strong>'.'<br>';
+echo '<input type="text" name="name">';
+$text='hello';
+//author: Messan Ayayi
+//date:23/02/2023
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <input type="text" value="<?php echo $text ?>">
+</body>
+</html>
